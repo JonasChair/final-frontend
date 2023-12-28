17ed0fc8ad19d46d57fe9edae314cc2db8722872
JonasChai <jonas.chairutdinovas@gmail.com>
2023-12-28 07:41:38 +0200

    cleaned up some styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -40,7 +40,7 @@ const Header = () => {
                             </Link>
                         </li>
                         <li>
-                            <button onClick={onLogout}>Log out</button>
+                            <button className={styles.logOutButton} onClick={onLogout}>Log out</button>
                         </li>
                     </>
                     ) :
